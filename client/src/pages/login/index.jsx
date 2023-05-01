abefc90560032adc01ef84b31439f5f27c88e6b6
Ahamed Fahmi <it21037306@my.sliit.lk>
2023-05-01 18:13:38 +0530

    Redesigned login page 😀

--- client/src/pages/login/index.jsx
+++ client/src/pages/login/index.jsx
@@ -1,5 +1,6 @@
-import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import { Box, Typography, useTheme, useMediaQuery, Grid } from "@mui/material";
 import Form from "./Form.jsx";
+import loginImage from "../../assets/image/login.jpg";
 
 const LoginPage = () => {
   const theme = useTheme();
@@ -17,15 +18,41 @@ const LoginPage = () => {
         </Typography>
       </Box>
 
-      <Box
-        width={isNonMobileScreens ? "40%" : "93%"}
-        p="2rem"
-        m="2rem auto"
-        borderRadius="1.5rem"
-        backgroundColor={theme.palette.background.alt}
-      >
-        <Form />
-      </Box>
+      <Grid container>
+        <Grid item xs={8}>
+          <Box
+            marginTop="3%"
+            marginLeft="2%"
+            component="img"
+            width={isNonMobileScreens ? "100%" : "93%"}
+            backgroundColor={theme.palette.background.alt}
+            borderRadius="1.5rem"
+            src={loginImage}
+          ></Box>
+        </Grid>
+        <Grid item xs={4}>
+          <Box
+            width={isNonMobileScreens ? "80%" : "93%"}
+            p="2rem"
+            marginTop="20%"
+            marginBottom="auto"
+            marginLeft="auto"
+            marginRight="auto"
+            borderRadius="1.5rem"
+            backgroundColor={theme.palette.background.alt}
+          >
+            <Typography
+              fontWeight="500"
+              variant="h2"
+              sx={{ mb: "1.5rem" }}
+              textAlign="center"
+            >
+              Welcome to TrashTaggers,
+            </Typography>
+            <Form />
+          </Box>
+        </Grid>
+      </Grid>
     </Box>
   );
 };
