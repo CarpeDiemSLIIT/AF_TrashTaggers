1000b19c20b56495fe8d34bef9cc83fee95d6c62
Isuru Herath <it21041648@my.sliit.lk>
2023-05-04 01:03:00 +0530

    cleanup

--- client/src/pages/events/Join.jsx
+++ client/src/pages/events/Join.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { Box, Button, Typography } from "@mui/material";
+import WidgetWrapper from "../../components/customMUI/WidgetWrapper";
+import { getAllEvents } from "../../features/events/eventSlice";
+import { useSelector, useDispatch } from "react-redux";
+
 
 function Join() {
 
 //Get unfinishedevent data and map down
 
+
+const dispatch = useDispatch();
+const { events, isError, message, isSuccess, isLoading } = useSelector(
+    (state) => state.event
+);
+
+useEffect(() => {
+    dispatch(getAllEvents());
+}, []);
+
+console.log(events);
+
+
   return (
-    <div>join</div>
+    <Box display="flex" flexDirection="column" gap="1rem">
+      {events.map((event) => (
+        <WidgetWrapper>
+          <Typography>{event.description}</Typography>
+        </WidgetWrapper>
+      ))}
+    </Box>
   )
 }
 
--- client/src/pages/events/index.jsx
+++ client/src/pages/events/index.jsx
@@ -1,24 +1,13 @@
 import React, { useEffect } from "react";
 import { Box, Button, Typography } from "@mui/material";
-import WidgetWrapper from "../../components/customMUI/WidgetWrapper";
-import { getAllEvents } from "../../features/events/eventSlice";
-import { useSelector, useDispatch } from "react-redux";
+
+
 
 const Event = () => {
 
 //TODO : Get event Data and map down
 
-const dispatch = useDispatch();
-const { events, isError, message, isSuccess, isLoading } = useSelector(
-    (state) => state.event
-);
 
-useEffect(() => {
-    dispatch(getAllEvents());
-}, []);
-
-
-console.log(events);
 
   return(
     
@@ -28,11 +17,7 @@ console.log(events);
     <br />
     <hr/>
     <Box display="flex" flexDirection="column" gap="1rem">
-      {events.map((event) => (
-        <WidgetWrapper>
-          <Typography>{event.description}</Typography>
-        </WidgetWrapper>
-      ))}
+      events
     </Box></div>
   ); 
 };
